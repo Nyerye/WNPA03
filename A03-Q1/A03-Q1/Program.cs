b87701a038519802c5f5228ad9cdcae49ab1acca
Nyerye <nick_reilly@outlook.com>
2026-03-19 13:56:41 -0400

    Tests made it worse.
    -Trying to use a setup method instead wjhere we use an instance of rand there and reference in BetterCode

--- A03-Q1/A03-Q1/Program.cs
+++ A03-Q1/A03-Q1/Program.cs
@@ -12,20 +12,26 @@ namespace A03_Q1
     {
 
         //Moving the initialization off the random class up here to save time
-        private Random rand = new Random();
+        private Random rand;
 
         static void Main(string[] args)
         {
 
             BenchmarkRunner.Run<Program>();
-          
+
         }
 
+        //Setting up the random object once so BetterCode can reuse it
+        [GlobalSetup]
+        public void Setup()
+        {
+            rand = new Random();
+        }
 
         [Benchmark]
         public void InitialCode()
         {
-            
+
             int temp = 0;
             Random rand = new Random();
             int LOOPCOUNT = 10000;
@@ -35,12 +41,12 @@ namespace A03_Q1
                 if (randomFloat < .10)
                 {
                     temp = 1;
-                    
+
                 }
                 if (randomFloat >= .10 && randomFloat < .30)
                 {
                     temp = 2;
-                    
+
                 }
                 if (randomFloat >= .3)
                 {
@@ -49,7 +55,7 @@ namespace A03_Q1
                 }
             }
             GC.KeepAlive(temp);
-            
+
         }
 
         [Benchmark]
@@ -61,11 +67,9 @@ namespace A03_Q1
             {
                 float randomFloat = rand.NextSingle();
                 temp = randomFloat < .10f ? 1 : randomFloat < .30f ? 2 : 3;
-
-
             }
             GC.KeepAlive(temp);
-           
+
         }
     }
 }
